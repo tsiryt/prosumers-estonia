2376ff864436dff604ebbbfc222d8d0cccb2baa5
tenihasina <sandratrarasendrasoa@gmail.com>
2024-01-25 07:24:49 +0100

    loading is now in a new file

---
+++ load_dataset.R
@@ -0,0 +1,11 @@
+# global variables
+
+CLIENT <- read_delim(here::here("data", "client.csv"))
+TRAIN <- read_delim(here::here("data", "train.csv"))
+GAS_PRICES <- read_delim(here::here("data", "gas_prices.csv"))
+ELECTRICITY_PRICES <- read_delim(here::here("data", "electricity_prices.csv"))
+FORECAST_WEATHER <- read_delim(here::here("data", "forecast_weather.csv"))
+ID_TO_COUNTY <- fromJSON(here::here("data", "county_id_to_name_map.json")) %>%
+    unlist() %>%
+    tibble::enframe() %>%
+    rename(id = name, county = value)
