88607662fe207b4057874905900ac1a789f504c7
Tsiry Rasendrasoa <rasendrasoatsiry@gmail.com>
2024-01-29 14:27:33 +0100

    Add hour as grouping variable for nested models

--- R/rf_feature_selection.R
+++ R/rf_feature_selection.R
@@ -1,27 +1,33 @@
 ### Random Forest feature selection ###
 here::i_am("R/rf_feature_selection.R")
 if (!exists("history_weather_county")){
-  source(here::here("init.R"))
+  source(here::here("R", "init.R"))
 }
 
 ntree <- 400
+# splitting train & test sets
+datetime_split <- ymd_hms("2023-01-24 00:00:00")
 
 history_weather_avg <- history_weather_county %>%
   group_by(datetime) %>%
   summarise(across(everything(), ~ mean(., na.rm = TRUE))) %>%
   select(-county)
 train_weather <- TRAIN %>%
-  filter(!prediction_unit_id %in% id_missing_values_in_train) %>%
+  filter(
+    !prediction_unit_id %in% id_missing_values_in_train,
+    datetime <= datetime_split
+  ) %>%
   group_by(is_consumption, prediction_unit_id) %>%
   mutate(
     # interpolate NAs at daylight saving time
     target = na.approx(target),
     is_consumption = as.factor(is_consumption),
     is_business = as.factor(is_business),
-    product_type = as.factor(product_type)
+    product_type = as.factor(product_type),
+    hour = hour(datetime)
   ) %>%
   ungroup() %>%
-  group_by(datetime, is_consumption) %>%
+  group_by(datetime, is_consumption, is_business) %>%
   summarise(across(where(is.numeric), ~ mean(., na.rm = TRUE))) %>%
   inner_join(
     history_weather_avg,
@@ -33,7 +39,7 @@ train_weather <- TRAIN %>%
     -starts_with("data_block"),
     -row_id,
     -prediction_unit_id,
-    -datetime,
+    # -datetime,
     -county
   )
 
@@ -67,7 +73,7 @@ toc()
 ### Visualize variable importance ----------------------------------------------
 
 # Get variable importance from the model fit
-ImpData <- rf_fit_prod$importance %>%
+ImpData <- rf_fit_cons$importance %>%
   as.data.frame() %>%
   rownames_to_column(var = "Var.Names") %>%
   as_tibble()
@@ -89,4 +95,7 @@ ggplot(ImpData, aes(x = Var.Names, y = `%IncMSE`)) +
 
 # Partial effects
 df_train <- data.frame(train_weather)
-partialPlot(rf_fit_prod, df_train, shortwave_radiation)
+partialPlot(rf_fit_cons, df_train, surface_pressure)
+
+# prod : shortwave_radiation, direct_solar_radiation, diffuse_radiation, dewpoint, cloudcover_total, surface_pressure, temperature
+# cons : temperature, surface_pressure, shortwave_radiation, direct_solar_radiation, diffuse_radiation, dewpoint, cloudcover_low, cloudcover_total
